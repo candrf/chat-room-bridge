4e6115fa8822661231659ffa16ddc1f4b7249840
Andrew France <andrew.c.france.mil@army.mil>
2025-10-20 16:52:22 -0500

    first user conroller test

--- src/main/java/org/france/chatroombridge/controllers/UserController.java
+++ src/main/java/org/france/chatroombridge/controllers/UserController.java
@@ -1,4 +1,28 @@
 package org.france.chatroombridge.controllers;
 
+
+import org.france.chatroombridge.entities.User;
+import org.france.chatroombridge.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    private final UserService userService;
+
+    UserController(UserService userService){
+        this.userService = userService;
+    }
+
+
+    @PostMapping()
+    public ResponseEntity<User> createUser(User user){
+        User savedUser = userService.saveUser(user);
+        return ResponseEntity.status(201).body(savedUser);
+    }
 }
--- src/test/java/org/france/chatroombridge/controllers/UserControllerTest.java
+++ src/test/java/org/france/chatroombridge/controllers/UserControllerTest.java
@@ -1,10 +1,59 @@
 package org.france.chatroombridge.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.france.chatroombridge.entities.User;
+import org.france.chatroombridge.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.JsonbHttpMessageConverter;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 @AutoConfigureMockMvc
 public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockitoBean
+    UserService userService;
+
+    User testUser = User.builder()
+            .id(1L)
+            .name("Andrew")
+            .build();
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void shouldCreateUser() throws Exception{
+        Mockito.when(userService.saveUser(testUser))
+                .thenReturn(testUser);
+
+        String userJson = objectMapper.writeValueAsString(testUser);
+
+        mvc.perform(post("/api/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userJson))
+                .andExpect(status().is2xxSuccessful());
+    }
 
 }
