4b6e1ea78414f95572c234b7e15e66444761b74b
Andrew France <andrew.c.france.mil@army.mil>
2025-10-20 13:39:25 -0500

    initial commit

---
+++ src/main/java/org/france/chatroombridge/ChatRoomBridgeApplication.java
@@ -0,0 +1,13 @@
+package org.france.chatroombridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatRoomBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/france/chatroombridge/ChatRoomBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.france.chatroombridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRoomBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
